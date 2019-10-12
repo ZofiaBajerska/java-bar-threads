439585828756c36d6cc36789067f889e430d47b2
Zosia <b.zocha@gmail.com>
2019-10-12 20:58:57 +0200

    Added Client class.

---
+++ src/main/java/model/Client.java
@@ -0,0 +1,33 @@
+package model;
+
+import environment.Bar;
+
+import static java.lang.Thread.*;
+
+public class Client implements Runnable {
+    private Bar bar;
+    private String name;
+
+    public Client(Bar bar, String name) {
+        this.bar = bar;
+        this.name = name;
+    }
+
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                String drink = bar.take();
+                System.out.println(name + " " + drink);
+                sleep(1000 * drink.length());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                break;
+            }
+
+        }
+
+    }
+}
+
